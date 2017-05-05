22e38b38f37ce9b8b0840a3102c5715e1345934b
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-05-05 09:42:43 -0700

    create a spec for all volunteers. create all class method for Volunteer class. spec will pass.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -6,4 +6,16 @@ class Volunteer
     @last_name = attributes.fetch(:last_name)
     @id = attributes[:id]
   end
+
+  def Volunteer.all
+    results = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    results.each() do |volunteer|
+      first_name = volunteer.fetch('first_name')
+      last_name = volunteer.fetch('last_name')
+      id = volunteer.fetch('id')
+      volunteers.push(Volunteer.new({:first_name => first_name, :last_name => last_name, :id => id}))
+    end
+    volunteers
+  end
 end
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -7,5 +7,15 @@ describe(Volunteer) do
       vol_Steve_Jobs = Volunteer.new({:first_name => 'Steve', :last_name => 'Jobs'})
       expect(vol_Steve_Jobs.first_name()).to(eq('Steve'))
     end
+    it('will create a new volunteer') do
+      vol_Steve_Jobs = Volunteer.new({:first_name => 'Steve', :last_name => 'Jobs'})
+      expect(vol_Steve_Jobs.last_name()).to(eq('Jobs'))
+    end
+  end
+
+  describe('.all') do
+    it('will return an empty array at the start') do
+      expect(Volunteer.all()).to(eq([]))
+    end
   end
 end
