bc51cdda3e4229dc9ffeb0e369abee692b344f14
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-05-05 09:13:45 -0700

    create spec for all method.create all method to return empty array at start. pass second spec.

--- lib/project.rb
+++ lib/project.rb
@@ -5,4 +5,15 @@ class Project
     @project_name = attributes.fetch(:project_name)
     @id = attributes[:id]
   end
+
+  def Project.all
+    results = DB.exec("SELECT * FROM projects;")
+    projects = []
+    results.each() do |project|
+      project_name = project.fetch('project_name')
+      id = project.fetch('id').to_i()
+      projects.push(Project.new({:project_name => project_name, :id => id}))
+    end
+    projects
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,10 +1,17 @@
 require'spec_helper'
 
 describe(Project) do
+
   describe('#initialize') do
     it('will create a new project') do
       project1 = Project.new({:project_name => 'Beach Clean Up', :id => 1})
       expect(project1.project_name()).to(eq('Beach Clean Up'))
+    end
+  end
+
+  describe('.all') do
+    it('will return an empty array') do
+      expect(Project.all()).to(eq([]))
     end
   end
 end
